43ac48906b3bd7314653a30950f39fd2969bf3bd
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.internal.salesforce.com>
2018-01-27 11:05:08 -0600

    Insert reservation Test 6

--- salesforce.js
+++ salesforce.js
@@ -58,10 +58,10 @@ let makeReservation = (session) => {
         rez.set('Tour__c', session.attributes.selectedTour.id);
         rez.set('Contact_First_Name__c', session.attributes.firstName);
         rez.set('Contact_Last_Name__c', session.attributes.lastName);
-        rez.set('Adults__c', session.attributes.Adults__c);
-        rez.set('Children__c', session.attributes.Children__c);
-        rez.set('Reservation_Date__c', session.attributes.date);
-        rez.set('Reservation_Time__c', session.attributes.time);
+        //rez.set('Adults__c', session.attributes.Adults__c);
+        //rez.set('Children__c', session.attributes.Children__c);
+        //rez.set('Reservation_Date__c', session.attributes.date);
+        //rez.set('Reservation_Time__c', session.attributes.time);
         org.insert({sobject: rez}, err => {
             if (err) {
                 console.error(err);
