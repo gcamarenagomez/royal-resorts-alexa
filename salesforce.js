545199829a0ba98234f153dc7841475c5db5c2a4
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.internal.salesforce.com>
2018-01-27 10:59:16 -0600

    Insert reservation Test 5

--- salesforce.js
+++ salesforce.js
@@ -55,13 +55,13 @@ let makeReservation = (session) => {
     console.log('Session ' + session.attributes.selectedTour.id);
     return new Promise((resolve, reject) => {
         let rez = nforce.createSObject('Tour_Reservation__c');
-        rez.set('Tour__c', `${session.attributes.selectedTour.id}`);
-        rez.set('Contact_First_Name__c', `${session.attributes.firstName}`);
-        rez.set('Contact_Last_Name__c', `${session.attributes.lastName}`);
-        rez.set('Adults__c', `${session.attributes.Adults__c}`);
-        rez.set('Children__c', `${session.attributes.Children__c}`);
-        rez.set('Reservation_Date__c', `${session.attributes.date}`);
-        rez.set('Reservation_Time__c', `${session.attributes.time}`);
+        rez.set('Tour__c', session.attributes.selectedTour.id);
+        rez.set('Contact_First_Name__c', session.attributes.firstName);
+        rez.set('Contact_Last_Name__c', session.attributes.lastName);
+        rez.set('Adults__c', session.attributes.Adults__c);
+        rez.set('Children__c', session.attributes.Children__c);
+        rez.set('Reservation_Date__c', session.attributes.date);
+        rez.set('Reservation_Time__c', session.attributes.time);
         org.insert({sobject: rez}, err => {
             if (err) {
                 console.error(err);
