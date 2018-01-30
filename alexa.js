20c08f49523e1c02ee1bac72d313af71d462ce39
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.internal.salesforce.com>
2018-01-30 12:08:51 -0600

    Amazon music integration

--- alexa.js
+++ alexa.js
@@ -56,7 +56,7 @@ module.exports = (req, res) => {
                 directives: directives
             }
         });
-        console.log(res);
+        console.log(res.json());
     };
 
     return {
